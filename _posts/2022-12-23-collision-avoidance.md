f7e5188579600dc8dcbe598ade511e890bb8233a
Max <max.ledlie@spartansolutions.com>
2022-12-28 13:28:54 +0000

    Agent B can have a non-zero velocity. Velocity obstacle is translated accordingly.

--- _posts/2022-12-23-collision-avoidance.md
+++ _posts/2022-12-23-collision-avoidance.md
@@ -20,11 +20,26 @@ Here is an example of this technique in action:
 
 Avoiding collision for all time is a daunting task. Instead, let's pick some window of time $\tau$ (for example, one second), and commit to avoiding any collisions in that window. Each agent wants to find a velocity that it can move with for the upcoming time window and be sure it will not collide.
 
-To make things as simple as possible, first suppose there are just two agents, $A$ and $B$. $B$ is just sitting still for now, and $A$ wants to know which velocities it can take. The authors use the concept of a *velocity obstacle* $VO^{\tau}_{A\|B}$. This is the set of velocities that $A$ can use that *will* result in a collision with $B$ within the time window $\tau$.
+To make things as simple as possible, first suppose there are just two agents, $A$ and $B$. $B$ is just sitting still for now, and $A$ wants to know which velocities it can take. The authors use the concept of a *velocity obstacle* $VO^{\tau}_{A\|B}$. This is the set of velocities that $A$ can use that *will* result in a collision with $B$ within the time window $\tau$. 
+
+The charts below provide an interactive example. The chart on the left represents the space of possible velocities agent A can take. The chart on the right displays the positions of agents A (blue) and B (red). Click a point in velocity space and  agent A will move with that velocity for one second before returning to its starting location. The red region in velocity space is $VO^{\tau}_{A\|B}$. Verify that velocities within this region cause A to collide with B.
 
 <div style='display: flex; width: 100%; height: 500px'>
   <div id='velocitySpace' style='flex: 1; padding-right: 5px;'></div>
   <div id='positionSpace' style='flex: 1; padding-left: 5px;'></div>
 </div>
+
+If agent B is also moving with some constant velocity $v_B$ in the time window, our velocity obstacle is correspondingly translated by the vector $v_B$. Right click somewhere in velocity space to set the velocity of agent B, and notice how the velocity obstacle is translated accordingly.
+
+Things get harder when we don't know for sure which velocity agent B will choose in the next time window. Suppose all we know is that agent B will choose from a set of velocities $V_B$. What is the set of velocities for agent A that will avoid a collision, *no matter which* velocity agent B selects from $V_B$?
+
+In the next interactive, an annotation shows the set $V_B$ in velocity space. When you select a $v_A$, agent B will try its best to choose a velocity from $V_B$ that will result in a collision.
+
+<div style='display: flex; width: 100%; height: 500px'>
+  <div id='velocitySpace2' style='flex: 1; padding-right: 5px;'></div>
+  <div id='positionSpace2' style='flex: 1; padding-left: 5px;'></div>
+</div>
+
+
 
 <script src="{{ base.url | prepend: site.url }}/assets/js/velocityObstacle.js">
--- assets/js/velocityObstacle.js
+++ assets/js/velocityObstacle.js
@@ -1,14 +1,16 @@
 // Constants
-const TAU = 1;
+const TAU = 2;
 const WAIT = 1;
 const AXIS_PADDING = 30;
 
 // Data
-let pointA = [20, 20];
+let pointA = [0, 0];
 const radiusA = 5;
 
 let pointB = [50, 70];
 const radiusB = 8;
+
+const vb = [-30, -50];
 
 function defineAxes(container, svg, targetDomain) {
   const size = container.node().getBoundingClientRect();
@@ -16,43 +18,50 @@ function defineAxes(container, svg, targetDomain) {
   const isPortrait = size.height > size.width;
   const aspectRatio = (size.width - 2 * AXIS_PADDING) / (size.height - 2 * AXIS_PADDING);
   
-  // Set up axes
-  const yAxis = svg.append('g')
-    .attr('transform', `translate(${AXIS_PADDING}, 0)`);
-  
-  const xAxis = svg.append('g')
-    .attr('transform', `translate(0, ${size.height - AXIS_PADDING})`);
-  
+  const truncateFactorX = isPortrait ? aspectRatio : 1;
   const x = d3.scaleLinear()
-    .domain([targetDomain[0], isPortrait ? targetDomain[1] * aspectRatio : targetDomain[1]])
+    .domain([truncateFactorX * targetDomain[0], truncateFactorX * targetDomain[1]])
     .range([AXIS_PADDING, size.width - AXIS_PADDING]);
   
-  const delta = x.copy().range([0, size.width - 2 * AXIS_PADDING]);
-  
+  const truncateFactorY = isPortrait ? 1 : aspectRatio;
   const y = d3.scaleLinear()
-    .domain([targetDomain[0], isPortrait ? targetDomain[1] : targetDomain[1] * aspectRatio])
+    .domain([truncateFactorY * targetDomain[0], truncateFactorY * targetDomain[1]])
     .range([size.height - AXIS_PADDING, AXIS_PADDING]);
+
+  const xDelta = d3.scaleLinear()
+    .domain([0, 1])
+    .range([0, x(1) - x(0)]);
+
+  const yDelta = d3.scaleLinear()
+    .domain([0, 1])
+    .range([0, y(1) - y(0)]);
 
   const axisLeft = d3.axisLeft(y);
   const axisBottom = d3.axisBottom(x);
+
+  const yAxis = svg.append('g')
+    .attr('transform', `translate(${x(0)}, 0)`);
+
+  const xAxis = svg.append('g')
+    .attr('transform', `translate(0, ${y(0)})`);
   
   yAxis.call(axisLeft);
   xAxis.call(axisBottom);
   
-  return [x, y, delta];
+  return [x, y, xDelta, yDelta];
 }
 
 const positionContainer = d3.select('#positionSpace');
 const positionSvg = positionContainer.append('svg')
   .attr('width', '100%')
   .attr('height', '100%');
-const [xPos, yPos, deltaPos] = defineAxes(positionContainer, positionSvg, [0, 100]);
+const [xPos, yPos, xDeltaPos, yDeltaPos] = defineAxes(positionContainer, positionSvg, [-50, 100]);
 
 const velocityContainer = d3.select('#velocitySpace');
 const velocitySvg = velocityContainer.append('svg')
   .attr('width', '100%')
   .attr('height', '100%');
-const [xVel, yVel, deltaVel] = defineAxes(velocityContainer, velocitySvg, [0, 80]);
+const [xVel, yVel, xDeltaVel, yDeltaVel] = defineAxes(velocityContainer, velocitySvg, [-50, 50]);
 
 // Draw the velocity obstacle
 const relPosX = pointB[0] - pointA[0];
@@ -68,21 +77,22 @@ const c = combinedRadiusSq - relPosY * relPosY;
 const disc = b * b - 4 * a * c;
 
 if (disc >= 0) {
-  const voGroup = velocitySvg.append('g');
+  const voGroup = velocitySvg.append('g')
+    .attr('transform', `translate(${xDeltaVel(vb[0])}, ${yDeltaVel(vb[1])})`)
 
   const m1 = (-b - Math.sqrt(disc)) / (2 * a);
   let alpha = 1 + m1 * m1;
   let beta = -2 * (relPosX + m1 * relPosY);
   const anchor1X = -beta / (2 * alpha * TAU);
-  const anchor1Y = m1 * anchor1X / TAU;
+  const anchor1Y = m1 * anchor1X;
 
   const m2 = (-b + Math.sqrt(disc)) / (2 * a);
   alpha = 1 + m2 * m2;
   beta = -2 * (relPosX + m2 * relPosY);
   const anchor2X = -beta / (2 * alpha * TAU);
-  const anchor2Y = m2 * anchor2X / TAU;
+  const anchor2Y = m2 * anchor2X;
 
-  const circleRadius = deltaVel(combinedRadius / TAU);
+  const circleRadius = xDeltaVel(combinedRadius / TAU);
 
   voGroup.append('path')
     .attr('d', `M ${xVel(anchor1X)} ${yVel(anchor1Y)}
@@ -90,22 +100,31 @@ if (disc >= 0) {
       L ${xVel(100)} ${yVel(m2 * 100)}
       L ${xVel(100)} ${yVel(m1 * 100)}
       Z`)
-    .style('stroke', '#800000')
+    .style('stroke', '#333333')
     .style('stroke-width', 2)
-    .style('fill', '#ff9999');
+    .style('fill', '#b3b3b3')
+    .style('opacity', 0.7);
 }
+
+// Mark the velocity of agent B
+velocitySvg.append('circle')
+  .attr('cx', xVel(vb[0]))
+  .attr('cy', yVel(vb[1]))
+  .attr('r', 3)
+  .style('fill', '#ff9999');
 
 const g = positionSvg.append('g');
 drawAgents();
 
 velocitySvg.on('click', function(event) {
   const agentA = g.select('#agentA');
+  const agentB = g.select('#agentB');
 
   const velX = xVel.invert(event.offsetX);
   const velY = yVel.invert(event.offsetY);
 
-  const finalPosX = pointA[0] + TAU * velX;
-  const finalPosY = pointA[1] + TAU * velY;
+  const finalPosA = [pointA[0] + TAU * velX, pointA[1] + TAU * velY];
+  const finalPosB = [pointB[0] + TAU * vb[0], pointB[1] + TAU * vb[1]];
 
   agentA.interrupt('move');
   agentA.interrupt('wait');
@@ -113,10 +132,23 @@ velocitySvg.on('click', function(event) {
   agentA.transition('move')
     .duration(TAU * 1000)
     .ease(d3.easeLinear)
-    .attr('cx', xPos(finalPosX))
-    .attr('cy', yPos(finalPosY))
+    .attr('cx', xPos(finalPosA[0]))
+    .attr('cy', yPos(finalPosA[1]))
     .on('end', () => {
       agentA.transition('wait')
+        .duration(WAIT * 1000)
+        .on('end', reset)
+        .on('interrupt', reset);
+    })
+    .on('interrupt', reset);
+
+  agentB.transition('move')
+    .duration(TAU * 1000)
+    .ease(d3.easeLinear)
+    .attr('cx', xPos(finalPosB[0]))
+    .attr('cy', yPos(finalPosB[1]))
+    .on('end', () => {
+      agentB.transition('wait')
         .duration(WAIT * 1000)
         .on('end', reset)
         .on('interrupt', reset);
@@ -128,6 +160,10 @@ function reset() {
   g.select('#agentA')
     .attr('cx', xPos(pointA[0]))
     .attr('cy', yPos(pointA[1]))
+
+  g.select('#agentB')
+    .attr('cx', xPos(pointB[0]))
+    .attr('cy', yPos(pointB[1]));
 }
 
 function drawAgents() {
@@ -138,15 +174,16 @@ function drawAgents() {
     .attr('id', 'agentA')
     .attr('cx', xPos(pointA[0]))
     .attr('cy', yPos(pointA[1]))
-    .attr('r', deltaPos(radiusA))
+    .attr('r', xDeltaPos(radiusA))
     .style('fill', '#9999ff')
     .style('stroke', '#000066')
     .style('stroke-width', 2);
 
   g.append('circle')
+    .attr('id', 'agentB')
     .attr('cx', xPos(pointB[0]))
     .attr('cy', yPos(pointB[1]))
-    .attr('r', deltaPos(radiusB))
+    .attr('r', xDeltaPos(radiusB))
     .style('fill', '#ff9999')
     .style('stroke', '#800000')
     .style('stroke-width', 2);
