e2ada2db2210d1f486678a095c758a2843858b11
Max <max.ledlie@spartansolutions.com>
2023-01-03 18:25:40 +0000

    Simplified code for rendering half plane boundaries

--- assets/js/linearProgramming.js
+++ assets/js/linearProgramming.js
@@ -1,6 +1,15 @@
 import GeometryCanvas from './GeometryCanvas.js';
 import Vector2 from './Vector2.js';
 
+// Represents the set of points (x, y) in R^2 such that a*x + b*y <= c
+class HalfPlane {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+}
+
 const DOMAIN = [0, 100];
 const GRADIENT_THICKNESS = 20;
 
@@ -31,6 +40,7 @@ let currentLine = null;
 let currentRect = null;
 let currentPerp = null;
 let dragStart = null;
+const halfPlanes = [];
 
 canvas.svg.on('mousedown', e => {
   e.stopPropagation();
@@ -46,6 +56,44 @@ canvas.svg.on('mousedown', e => {
   currentPerp = canvas.line(dragStart, dragStart, {'stroke': 'red', 'stroke-width': 2});
 });
 
+function updateCurrentHalfPlane(halfPlane, displayLine, displayRect) {
+  const { a, b, c } = {...halfPlane};
+
+  // Special case for vertical line
+  if (b === 0) {
+    displayLine
+      .attr('x1', canvas.xScale(dragStart.x))
+      .attr('y1', canvas.yScale(DOMAIN[0] - 10))
+      .attr('x2', canvas.xScale(dragStart.x))
+      .attr('y2', canvas.yScale(DOMAIN[1] + 10));
+
+    return;
+  }
+
+  const gradient = -a / b;
+  const intercept = c / b;
+  const lineStartX = DOMAIN[0] - 10;
+  const lineStart = new Vector2(lineStartX, gradient * lineStartX + intercept);
+  const lineEndX = DOMAIN[1] + 10;
+  const lineEnd = new Vector2(lineEndX, gradient * lineEndX + intercept);
+
+  displayLine
+    .attr('x1', canvas.xScale(lineStart.x))
+    .attr('y1', canvas.yScale(lineStart.y))
+    .attr('x2', canvas.xScale(lineEnd.x))
+    .attr('y2', canvas.yScale(lineEnd.y));
+
+  const theta = 180 * Math.atan2(b, a) / Math.PI;
+
+  displayRect
+    .attr('width', 2 * GRADIENT_THICKNESS)
+    .attr('height', canvas.yDeltaScale(1000))
+    .attr('x', canvas.xScale(dragStart.x) - GRADIENT_THICKNESS)
+    .attr('y', canvas.yScale(dragStart.y + 200))
+    .attr('transform', `rotate(${-theta}, ${canvas.xScale(dragStart.x)}, ${canvas.yScale(dragStart.y)})`)
+    .style('fill', 'url(#myGrad)');
+}
+
 canvas.svg.on('mousemove', e => {
   if (currentLine === null) return;
   
@@ -57,48 +105,15 @@ canvas.svg.on('mousemove', e => {
   const currentPerpVec = new Vector2(
     canvas.xScale.invert(e.offsetX) - dragStart.x,
     canvas.yScale.invert(e.offsetY) - dragStart.y
-  );
+  ).normalized();
 
-  const currentLineVec = new Vector2(
-    currentPerpVec.y,
-    -currentPerpVec.x
-  );
+  const a = currentPerpVec.x;
+  const b = currentPerpVec.y;
+  const c = a * dragStart.x + b * dragStart.y;
 
-  // Special case for vertical line
-  if (currentLineVec.x === 0) {
-    currentLine
-      .attr('x1', canvas.xScale(dragStart.x))
-      .attr('y1', canvas.yScale(DOMAIN[0] - 10))
-      .attr('x2', canvas.xScale(dragStart.x))
-      .attr('y2', canvas.yScale(DOMAIN[1] + 10));
+  const halfPlane = new HalfPlane(a, b, c);
 
-    return;
-  }
-
-  const selectedLineGrad = currentLineVec.y / currentLineVec.x;
-  const lineStartX = DOMAIN[0] - 10;
-  const lineStartY = dragStart.y - selectedLineGrad * (dragStart.x - lineStartX);
-  const lineStart = new Vector2(lineStartX, lineStartY);
-  const lineEndX = DOMAIN[1] + 10;
-  const lineEnd = new Vector2(lineEndX, dragStart.y - selectedLineGrad * (dragStart.x - lineEndX));
-
-  console.log(`selectedLineGrad: ${selectedLineGrad}`);
-
-  currentLine
-    .attr('x1', canvas.xScale(lineStart.x))
-    .attr('y1', canvas.yScale(lineStart.y))
-    .attr('x2', canvas.xScale(lineEnd.x))
-    .attr('y2', canvas.yScale(lineEnd.y));
-
-  const theta = 180 * Math.atan2(currentPerpVec.y, currentPerpVec.x) / Math.PI;
-
-  currentRect
-    .attr('width', 2 * GRADIENT_THICKNESS)
-    .attr('height', canvas.yDeltaScale(1000))
-    .attr('x', canvas.xScale(dragStart.x) - GRADIENT_THICKNESS)
-    .attr('y', canvas.yScale(dragStart.y + 200))
-    .attr('transform', `rotate(${-theta}, ${canvas.xScale(dragStart.x)}, ${canvas.yScale(dragStart.y)})`)
-    .style('fill', 'url(#myGrad)');
+  updateCurrentHalfPlane(halfPlane, currentLine, currentRect);
 });
 
 canvas.svg.on('mouseup', e => {
